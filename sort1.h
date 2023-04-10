af369438aafd0d3e1c9208fdd5199b155bf01eda
159www <70989276+159www@users.noreply.github.com>
2023-04-10 18:01:39 +0800

    Create sort1.h

---
+++ sort1.h
@@ -0,0 +1,11 @@
+//
+// Created by sgt on 2023/4/9.
+//选择排序的实现
+//
+#ifndef INC_1_SORT_H
+#define INC_1_SORT_H
+//选择排序
+void sort(int Array[],int Len);
+//返回数组区域的最小值下标
+int GetMinIndex(int Array[],int StartIndex,int EndIndex);
+#endif //INC_1_SORT_H
